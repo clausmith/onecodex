8a0d3f0b08d6c5e6332cd50c914b10f510e87ea0
Christopher Smith <christopher@sunsetroad.ca>
2017-07-28 16:55:39 -0700

    Adds classes for representing collections of samples, and method for generating OTU table

--- onecodex/models/sample.py
+++ onecodex/models/sample.py
@@ -11,6 +11,44 @@ from onecodex.models.misc import Projects, Tags
 from onecodex.models.helpers import truncate_string
 from onecodex.lib.upload import upload  # upload_file
 
+class OneCodexBaseCollection(object):
+    model = OneCodexBase
+
+    def __init__(self, items, *args, **kwargs):
+        self._items = items
+
+    def __iter__(self):
+        for x in list.__iter__(self._items):
+            yield x
+
+    def append(self, item):
+        if not isinstance(item, self.model):
+            raise AttributeError
+        self._items.append(item)
+        
+
+    def extend(self, items):
+        if not isinstance(items, self.__class__):
+            raise AttributeError
+        self._items.extend(items._items)
+
+    def __setitem__(self, index, item):
+        if not isinstance(item, self.model):
+            raise AttributeError
+        self._items[index] = item
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self._items[key]
+        return self.__class__(self._items[key])  # Slice
+
+    def __len__(self):
+        return len(self._items)
+
+    def __repr__(self):
+        # FIXME: Special case for 1 item
+        return '<{} with {} items [{}...{}]>'.format(self.__class__.__name__, len(self._items), self._items[0].__repr__(), self._items[-1].__repr__())
+
 
 class Samples(OneCodexBase):
     _resource_path = '/api/v1/samples'
@@ -94,7 +132,7 @@ class Samples(OneCodexBase):
             # TODO: implement this (see above block)
             samples = metadata_samples
 
-        return samples[:limit]
+        return SampleCollection(samples[:limit])
 
     @classmethod
     def search_public(cls, *filters, **keyword_filters):
@@ -164,6 +202,68 @@ class Samples(OneCodexBase):
             else:
                 raise OneCodexException('Download failed with an HTTP status code {}.'.format(
                                         exc.response.status_code))
+
+
+class SampleCollection(OneCodexBaseCollection):
+    model = Samples
+
+    def to_otu_table(self, metric='abundance', verbose=False):
+        import pandas as pd
+        """For a set of samples, return the results as a Pandas DataFrame."""
+        assert metric in ['abundance', 'readcount', 'readcount_w_children']
+        
+        # Keep track of all of the microbial abundances
+        dat = {}
+        # Keep track of information for each tax_id
+        tax_id_info = {}
+        
+        # Get results for each of the Sample objects that are passed in
+        for s in self._items:
+            # Get the primary classification for this sample
+            a = s._api.Classifications.get(str(s.primary_classification.id))
+            
+            if a.success is False:
+                if verbose:
+                    print("Analysis for {} did not succeed, skipping".format(s.filename))
+                continue
+                
+            # Get the results in table format
+            result = a.results()['table']
+            
+            # Record the information (name, parent) for each organism by  its tax ID
+            for d in result:
+                if d['tax_id'] not in tax_id_info:
+                    tax_id_info[d['tax_id']] = {k: d[k] for k in ['name', 'rank', 'parent_tax_id']}
+
+            # Reformat detection infromation as dict of {taxid: value}
+            result = {d['tax_id']: d[metric] for d in result if metric in d}
+
+            # Remove entries without the specified metric
+            result = {taxid: value for taxid, value in result.items() if value is not None}
+
+            # Remove about 0-values
+            result = {taxid: value for taxid, value in result.items() if value > 0}
+            
+            # Catch any samples that don't have the specified metric
+            if len(result) == 0:
+                if verbose:
+                    print("{} has no entries for {}, skipping.".format(s.filename, metric))
+                continue
+
+            # Save the set of microbial abundances
+            dat[str(s.id)] = result
+        if len(dat) == 0:
+            return None
+
+        # Format as a Pandas DataFrame
+        df = pd.DataFrame(dat).T.fillna(0)
+        
+        # Remove columns (tax_ids) with no values that are > 0
+        df = df.loc[:, df.sum() > 0]
+        
+        if verbose:
+            print("Returning a data frame with {} samples and {} tax_ids".format(df.shape[0], df.shape[1]))
+        return df, tax_id_info
 
 
 class Metadata(OneCodexBase):
